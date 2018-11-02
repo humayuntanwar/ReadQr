63e8b77cef998f801c9cb59251c010f8fd908344
humayuntanwar <humayuntanwar@gmail.com>
2018-11-02 12:41:06 +0500

    change 05..0

--- app/src/main/java/com/example/muhammadhumayun/readqr/ScannerActivity.java
+++ app/src/main/java/com/example/muhammadhumayun/readqr/ScannerActivity.java
@@ -10,7 +10,7 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class ScannerActivity extends AppCompatActivity  implements ZXingScannerView.ResultHandler{
     private ZXingScannerView mScannerView;
-    String resultString, tag,length,value;
+    String resultString, tag0,length0,value0;
     String TLV;
     public HashMap<String,String> lengthMap= new HashMap<String, String>();
 
@@ -29,15 +29,20 @@ public class ScannerActivity extends AppCompatActivity  implements ZXingScannerV
         /*
         02 to 51 Merchant Account Information
          */
-        lengthMap.put("02","PAYEE IDENTIFIER ACCOUNT NUM");
+        lengthMap.put("02","MERCHANTIDENTIFIERVISA");
+        lengthMap.put("03","MERCHANTIDENTIFIERVISA");
         lengthMap.put("04","MERCHANT IDENTIFIER MASTER CARD");
         lengthMap.put("05","MERCHANT IDENTIFIER MASTER CARD");
-        lengthMap.put("06","");
-        lengthMap.put("07","");
-        lengthMap.put("08","");
-        lengthMap.put("09","");
+        lengthMap.put("06","MERCHANTIDENTIFIER NPCI");
+        lengthMap.put("07","MERCHANTIDENTIFIER NPCI");
+        lengthMap.put("08","Reserved by EMVCo");
+        lengthMap.put("09","Reserved for Discover");
+        lengthMap.put("26","PAY PAK REGISTERED");
+        lengthMap.put("27","PAY PAK REGISTERED");
 
 
+
+        // continue till 51
 
 
         lengthMap.put("52","MERCHANT CATEGORY CODE");
@@ -74,57 +79,112 @@ public class ScannerActivity extends AppCompatActivity  implements ZXingScannerV
         resultString =  result.toString(); //this string is the resulting string of scanned QR
        // showData(resultString); // pass the string to show data
 
-        //Handling string result tag one
-        int startlen =0;
-        tag = resultString.substring(startlen, 2);  //tag first two tag
-        length = resultString.substring(2,4);
-        int lenthof1 = length.length();
-        String tagmap = lengthMap.get(tag);
-        String value = resultString.substring(4,lenthof1 +4);// is either 01 or 02 , type 1 qr or type 2 qr
-        int vallen1 = value.length() + tag.length() +lenthof1;
+        //Handling string result tag 00
+        int startlen0 =0;
+        tag0 = resultString.substring(startlen0, 2);  //tag first two tag
+        length0 = resultString.substring(2,4);
+        int lenthof0 = length0.length();
+        String tagmap0 = lengthMap.get(tag0);
+        String value = resultString.substring(4,lenthof0 +4);// is either 01 or 02 , type 1 qr or type 2 qr
+        int vallen0 = value.length() + tag0.length() +lenthof0;
 
 
-        //handling string result tag two
-        int startlen2 =  vallen1 ; // 2 BELONGS POINT INITIATION METHOD
-        String tag2 = resultString.substring(startlen2, startlen2+2);
-        String len2 = resultString.substring(startlen2+2,startlen2+4);
-        int lenof2 = len2.length();
-        String tagmap2 = lengthMap.get(tag2);
-        String val2 = resultString.substring(startlen2+4,lenof2+startlen2+4);// is either 12 dynamic QR , 11 static QR
-        int vallen2 = tag2.length() +lenof2 + val2.length();
+        //handling string result tag 01
+        int startlen1 =  vallen0 ; // 2 BELONGS POINT INITIATION METHOD
+        String tag1 = resultString.substring(startlen1, startlen1+2);
+        String len1 = resultString.substring(startlen1+2,startlen1+4);
+        int lenof1 = len1.length();
+        String tagmap1 = lengthMap.get(tag1);
+        String val1 = resultString.substring(startlen1+4,lenof1+startlen1+4);// is either 12 dynamic QR , 11 static QR
+        int vallen1 = tag1.length() +lenof1 + val1.length();
 
 
-        //handling starting result tag three
-        int startlen3 =  vallen2 +startlen2; //3 BELONGS TO MASTERS
-        String tag3 = resultString.substring(startlen3, startlen3+2);
-        String len3 = resultString.substring(startlen3+2,startlen3+4);
-        int lenof3 = Integer.parseInt(len3);
-        String tagmap3 = lengthMap.get(tag3);
-        String val3= resultString.substring(startlen3+4,lenof3+startlen3+4);// is either 12 dynamic QR , 11 static QR
-        int vallen3= tag3.length() +len3.length() + val3.length();
+
+        //handling string result tag 04
+        int startlen4 =  vallen1 ; // 2 BELONGS POINT INITIATION METHOD
+        String tag4 = resultString.substring(startlen4, startlen4+2);
+        String len4 = resultString.substring(startlen4+2,startlen4+4);
+        int lenof4 = len4.length();
+        String tagmap4 = lengthMap.get(tag4);
+        String val4 = resultString.substring(startlen4+4,lenof4+startlen4+4);// is either 12 dynamic QR , 11 static QR
+        int vallen4 = tag4.length() +lenof4 + val4.length();
+
+
+
+
+
+
+        //handling starting result tag 05
+        int startlen5 =  vallen4 +startlen4; //3 BELONGS TO MASTERS
+        String tag5 = resultString.substring(startlen5, startlen5+2);
+        String len5 = resultString.substring(startlen5+2,startlen5+4);
+        int lenof5 = Integer.parseInt(len5)     /*      *2 */ ; //THE LENGTH OF VALUE IS 2 TIMES THE GIVEN LENGTH, ERROR IN THE 1LINK CODE
+        String tagmap5 = lengthMap.get(tag5);
+        String val5= resultString.substring(startlen5+4,lenof5+startlen5+4);// is either 12 dynamic QR , 11 static QR
+        int vallen5= tag5.length() +len5.length() + val5.length();
        // String view = tag3+val3+len3;
 
 
 
-        //handling starting result tag four
+        //handling starting result tag 06
+        //change all the value onwards to 06..07..08..09
         int startlen4 =  vallen3 + startlen3; // 4 BELONGS TO MASTERS
         String tag4 = resultString.substring(startlen4, startlen4+2);
         String len4 = resultString.substring(startlen4+2,startlen4+4);
-        int lenof4 = Integer.parseInt(len4);
+        int lenof4 = Integer.parseInt(len4)  /*  +2 */ ; // THE LENGTH OF VALUE IS 2 CHAR LESS THEN THE ACTUAL LENGTH OF VALUE
         String tagmap4 = lengthMap.get(tag4);
         String val4= resultString.substring(startlen4+4,lenof4+startlen4+4);// mercchant account number
         int vallen4= tag4.length() +len4.length() + val4.length();
 
 
+
+
+
+
+        //handling starting result tag five
+        int startlen5 =  vallen4 + startlen4; // 4 BELONGS TO MASTERS
+        String tag5 = resultString.substring(startlen5, startlen5+2);
+        String len5 = resultString.substring(startlen5+2,startlen5+4);
+        int lenof5 = Integer.parseInt(len5)      ; // THE LENGTH OF VALUE IS 2 CHAR LESS THEN THE ACTUAL LENGTH OF VALUE
+        String tagmap5 = lengthMap.get(tag5);
+        String val5= resultString.substring(startlen5+4,lenof5+startlen5+4);// mercchant account number
+        int vallen5= tag5.length() +len5.length() + val5.length();
+
+
+
+
+        //handling starting result tag SIX
+        int startlen6 =  vallen5 + startlen5; // 4 BELONGS TO MASTERS
+        String tag6 = resultString.substring(startlen6, startlen6+2);
+        String len6 = resultString.substring(startlen6+2,startlen6+4);
+        int lenof6 = Integer.parseInt(len6)      ; // THE LENGTH OF VALUE IS 2 CHAR LESS THEN THE ACTUAL LENGTH OF VALUE
+        String tagmap6 = lengthMap.get(tag6);
+        String val6= resultString.substring(startlen6+4,lenof6+startlen6+4);// mercchant account number
+        int vallen6= tag6.length() +len6.length() + val6.length();
+
+
+
+
+
+        /*
+
+
+
+
+        REMOVE THE  *2 AND +2 AND ADD 76 BELOW TO MAKE IT M=WORK WITH OTHER CODE
+
+
         //lets now skip the merchant tags and go directly to handle 52
 
-        int startlen52 = startlen4 + vallen4+76; // 52 IS MCC // 76 chars from string skipped to reach 52
+        int startlen52 = startlen4 + vallen4; // 52 IS MCC // 76 chars from string skipped to reach 52
         String tag52 = resultString.substring(startlen52, startlen52+2);
         String len52= resultString.substring(startlen52+2,startlen52+4);
         int lenof52 = Integer.parseInt(len52);
         String tagmap52 = lengthMap.get(tag52);
         String val52= resultString.substring(startlen52+4,lenof52+startlen52+4);// amount
         int vallen52= tag52.length() +len52.length() + val52.length();
+
+
 
         //handling 53
         int startlen53 = startlen52 + vallen52; // 53 IS CURRENCY CODE
@@ -190,6 +250,7 @@ public class ScannerActivity extends AppCompatActivity  implements ZXingScannerV
 
 
 
+*/
 
 
 
@@ -205,18 +266,23 @@ public class ScannerActivity extends AppCompatActivity  implements ZXingScannerV
 
 
 
-        MainActivity.tvCardText.setText(tag+ "\nindicates:     "  +tagmap +"\nvalue is "+ value+"\n"+
+
+        MainActivity.tvCardText.setText(tag0+ "\nindicates:     "  +tagmap0 +"\nvalue is "+ value0+"\n"+
 
                                        tag2+ "\n indicates    "+tagmap2 + "\nvalue is : "+val2
                                        + "\n" + tag3 +"\n indicates  "+ tagmap3 + "\nvalue is : "+val3
                                         +"\n"+tag4 + "\n indicates    "+tagmap4 +"\n value is : " + val4
-                                        +"\n"+tag52 + "\n indicates  "+tagmap52 + "\n value is : "+val52
-                                        + "\n" + tag53+ "\nindicates  "+tagmap53+"\n value is : "+ val53
+                                          +"\n"+tag5 + "\n indicates    "+tagmap5 +"\n value is : " + val5
+                                           +"\n"+tag6 + "\n indicates    "+tagmap6 +"\n value is : " + val6
+
+
+            /*    +"\n"+tag52 + "\n indicates  "+tagmap52 + "\n value is : "+val52
+                                       + "\n" + tag53+ "\nindicates  "+tagmap53+"\n value is : "+ val53
                                          + "\n" + tag54+ "\nindicates  "+tagmap54+"\n value is : "+ val54
                                          + "\n" + tag58+ "\nindicates  "+tagmap58+"\n value is : "+ val58
-                                        + "\n" + tag59+ "\nindicates  "+tagmap59+"\n value is : "+ val59
-                                         + "\n" + tag60+ "\nindicates  "+tagmap60+"\n value is : "+ val60
+                                        + "\n" + tag59+ "\nindicates  "+tagmap59+"\n value is : "+ val59                                         + "\n" + tag60+ "\nindicates  "+tagmap60+"\n value is : "+ val60
                                             + "\n" + tag63+ "\nindicates  "+tagmap63+"\n value is : "+ val63
+                                            */
         ) ;
         onBackPressed();
 
